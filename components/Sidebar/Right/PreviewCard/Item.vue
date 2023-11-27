47c9d6a0cc5acb28d108d6253395953f923d0ac5
Leo Miranda <leonardomir@gmail.com>
2023-11-27 18:35:55 -0400

    refactor: change button

--- components/Sidebar/Right/PreviewCard/Item.vue
+++ components/Sidebar/Right/PreviewCard/Item.vue
@@ -1,10 +1,10 @@
 <template>
-	<button
+	<div
 		class="w-full text-left p-3 border-b hover:bg-gray-100 dark:hover:bg-dim-300 dark:text-white"
 		:class="[defaultTransition, twitterBorderColor]"
 	>
 		<slot></slot>
-	</button>
+	</div>
 </template>
 
 <script setup>
